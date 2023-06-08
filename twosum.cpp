8c8279a5723b438b86fc5ecdc09810d369bad8a8
itshimashis <111703927+itshimashis@users.noreply.github.com>
2023-06-08 23:10:09 +0530

    Update twosum.cpp

--- twosum.cpp
+++ twosum.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int>ans;
-        unordered_map<int,int>mp;
-        for(int i=0;i<nums.size();i++){
-            if(mp.find(nums[i])==mp.end()){
-                mp[nums[i]]=i;
+       // vector<int>ans;
+       vector<pair<int,int>>v;
+       for(int i=0;i<nums.size();i++) v.push_back(make_pair(nums[i],i));
+       sort(v.begin(),v.end());
+        int left=0;
+        int right=nums.size()-1;
+        while(right>left){
+            if(v[left].first+v[right].first==target) return {v[left].second,v[right].second};
+            else if(v[left].first+v[right].first>target){
+                right--;
             }
-            if(mp.find(target-nums[i])!=mp.end() && i!=mp[target-nums[i]]){
-                ans.push_back(i);
-                 ans.push_back(mp[target-nums[i]]);
-                
+            else{
+                left++;
             }
-            
         }
-        return ans;
-        
+        return {v[left].second,v[right].second};
     }
 };
